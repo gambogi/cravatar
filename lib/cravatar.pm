f749248ef8709e62fa49ddf21390140a746ca75e
William Orr <will@worrbase.com>
2013-09-18 23:46:30 -0700

    Added file uploading

--- lib/cravatar.pm
+++ lib/cravatar.pm
@@ -3,6 +3,7 @@ package cravatar;
 use Dancer ':syntax';
 use Dancer::Plugin::LDAP;
 use Dancer::Plugin::Cache::CHI;
+use MIME::Type;
 use Net::LDAP::Util qw/escape_filter_value/;
 use Try::Tiny;
 
@@ -28,6 +29,55 @@ get '/:UUID.jpg' => sub {
     my $photo = $ret->[0];
     cache_set $uuid, $photo;
     cache_page send_file(\$photo, content_type => 'image/jpeg');
+};
+
+get '/upload' => sub {
+    my $user = request->header('X-WEBAUTH-USER');
+    return template 'upload.tt', {
+        user => $user // "worr",
+    };
+};
+
+post '/upload' => sub {
+    my $ldap = ldap;
+    my $user = request->header('X-WEBAUTH-USER') or
+        return template 'error', {
+            message => 'Must log in with webauth',
+        };
+
+    my $file = request->upload('photo') or
+        return template 'error', {
+            message => 'Must provide a photo'
+        };
+
+    if ($file->type ne "image/jpeg") {
+        return template 'error', {
+            message => 'Must provide a jpeg image'
+        };
+    }
+
+    my $ret = $ldap->modify("uid=$user,ou=Users,dc=csh,dc=rit,dc=edu",
+        changes => [
+            replace => [
+                jpegPhoto => $file->content,
+            ],
+        ],
+    );
+
+    if ($ret->code) {
+        warning $ret->error;
+        return template 'error', {
+            message => $ret->error,
+        };
+    }
+
+    redirect '/upload/success';
+};
+
+get '/upload/success' => sub {
+    return template 'success', {
+        message => 'Success!',
+    };
 };
 
 true;
